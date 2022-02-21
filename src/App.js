2d53eaaff291252fd98e3414856611478c21de00
sntmsc <santiago.hamber@gmail.com>
2022-02-20 21:01:08 -0300

    add styles

--- src/App.js
+++ src/App.js
@@ -1,97 +1,17 @@
-import {useState} from 'react'
+import React from 'react'
 import './App.css';
-import { Text, Flex, Button } from '@chakra-ui/react'
-
-
-const Bpm = () => {
-const [average, setAverage] = useState(0);
-const [initTime, setInitTime] = useState({
-  time: 0,
-  acc:0
-});
-
-  let currentTime = 0;
-  let duration = 0;
-
-  const handleClick = () => {
-
-    if(initTime.acc === 0){
-      setInitTime({...initTime,
-        time: new Date().getTime(),
-        acc:initTime.acc+1});
-
-    }
-    else{
-      setInitTime({...initTime,
-        acc: initTime.acc+1});
-    }
-
-    currentTime = new Date().getTime();
-    duration = (currentTime - initTime.time)/1000;
-    
-    if(duration !== 0) setAverage((initTime.acc * 60 / duration).toFixed(1));
-
-    console.log(initTime.acc)
-
-  }
-
-  const Reset = () =>{
-
-     currentTime = 0;
-     duration = 0;
-     setAverage(0);
-     setInitTime({
-      time: 0,
-      acc:0
-    });
-  }
-
-  return(
-    <Flex
-    w='100%'
-    justify='center'
-    align='center'
-    direction='column'>
-      <Text
-      fontSize='3em'
-      userSelect = 'none'>
-        BPM meter
-      </Text>
-      <Flex
-      direction='column'
-      mt='2em'
-      boxSize='8em'
-      boxShadow='xl'
-      border='1px solid'
-      borderRadius='50%'
-      _hover={{cursor:'pointer'}}
-      bg='gray'
-      p='1em'
-      justify='center'
-      align='center'
-      onClick={handleClick}>
-      <Text
-      fontSize={initTime.acc === 0 ? '1em' : '2em'}
-      userSelect = 'none'>
-        {initTime.acc === 0 ? 'Press me' : average}
-      </Text>
-      </Flex>
-      <Button
-      colorScheme='teal'
-      variant='solid'
-      mt='1em'
-      onClick={Reset}>
-        Reset
-      </Button>
-    </Flex>
-  )
-}
+import { Flex } from '@chakra-ui/react'
+import Bpm from './components/tools/Bpm';
 
 function App() {
  
   return (
     <Flex
-    w='100%'>
+    backgroundImage="linear-gradient( #c6ffdd, #fbd786, #f7797d)"
+    w='100%'
+    h='100vh'
+    align='center'
+    justify='center'>
       <Bpm/>
     </Flex>
   );
